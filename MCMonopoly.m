ed5705d4b654aa2d8000131d95899ab0e3547a69
Spencer Bliven <spencer.bliven@gmail.com>
2012-01-13 11:48:04 -0800

    Switch to a 'roll, then move' model of time. Probabilities now reflect where the player will end the turn, rather than intermediate steps. This mostly effects Chance and Go-to-Jail spaces.
    Also cleaned up the code somewhat.

--- MCMonopoly.m
+++ MCMonopoly.m
@@ -1,18 +1,5 @@
 %% Script to run Markov Chain monopoly statistics
 
-x0 = eye(1,40); % probability of being on each square
-A = zeros(40); % transition probabilities from dice rolls
-
-% Initialize transition matrix for dice movement
-A(1,3:13) = [1:6, 5:-1:1]/36;
-for i = 2:40,
-    A(i,:) = circshift(A(i-1,:),[0 1]);
-end
-
-% Update probabilities for movement via Chance and Community Chest cards. 
-% This uses cards from the 'Here & Now' edition, but names have been translated to the traditional version.
-% Cards are assumed to be drawn uniformly at random with replacement.
-
 squares = {
     'GO';               %1
     'Mediterranean';
@@ -56,23 +43,39 @@ squares = {
     'Boardwalk'
 };
 
+
+D = zeros(40); % transition probabilities for dice movement
+
+% Initialize transition matrix for dice movement
+D(1,3:13) = [1:6, 5:-1:1]/36;
+for i = 2:40,
+    D(i,:) = circshift(D(i-1,:),[0 1]);
+end
+
+% Update probabilities for non-dice movement.
+ND = eye(40);
+
 % Go directly to jail
-jailSquare = 11;
-A(31,:) = 0;
-A(31,jailSquare) = 1;
+jailSquare = find(ismember(squares,'Jail'));
+ND(31,:) = 0;
+ND(31,jailSquare) = 1;
+
+% This uses cards from the 'Here & Now' edition, but the traditional names
+% are used.
+% Cards are assumed to be drawn uniformly at random with replacement.
 
 % Community Chest cards: 16 total
 % 15 Unchanged
 chestSquares = find(ismember(squares,'Community Chest'));
-A(chestSquares,:) = A(chestSquares,:)*15/16;
+ND(chestSquares,:) = ND(chestSquares,:)*15/16;
 % 1 Go directly to Jail
-A(chestSquares,jailSquare) = A(chestSquares,jailSquare)+1/16;
+ND(chestSquares,jailSquare) = ND(chestSquares,jailSquare)+1/16;
 
 
 % Chance cards: 16 total
 % 6 Unchanged
 chanceSquares= find(ismember(squares,'Chance'));
-A(chanceSquares,:) = A(chanceSquares,:)*6/16;
+ND(chanceSquares,:) = ND(chanceSquares,:)*6/16;
 % 1 Advance to GO
 % 1 Advance to Reading Railroad
 % 1 Go directly to Jail
@@ -81,41 +84,40 @@ A(chanceSquares,:) = A(chanceSquares,:)*6/16;
 % 1 Advance to Boardwalk
 dest = find(ismember(squares,{
     'GO','Reading Railroad','Jail','St. Charles','Illinois','Boardwalk'}));
-A(chanceSquares,dest) = A(chanceSquares,dest) + 1/16;
+ND(chanceSquares,dest) = ND(chanceSquares,dest) + 1/16;
 % 1 Go back 3 spaces
-A(chanceSquares,chanceSquares-3) = A(chanceSquares,chanceSquares-3) + eye(3)/16;
+ND(chanceSquares,chanceSquares-3) = ND(chanceSquares,chanceSquares-3) + eye(3)/16;
 % 1 Advance to nearest utility
-A(8,13) = A(8,13) + 1/16;
-A(23,29) = A(23,29) + 1/16;
-A(37,29) = A(37,29) + 1/16;
+ND(8,13) = ND(8,13) + 1/16;
+ND(23,29) = ND(23,29) + 1/16;
+ND(37,29) = ND(37,29) + 1/16;
 
 % 2 Advance to nearest railroad
-A(8,6) = A(8,6) + 2/16;
-A(23,26) = A(23,26) + 2/16;
-A(37,36) = A(37,36) + 2/16;
+ND(8,6) = ND(8,6) + 2/16;
+ND(23,26) = ND(23,26) + 2/16;
+ND(37,36) = ND(37,36) + 2/16;
 
 
+% A turn consists of a dice roll and then some non-dice movement
+A = D*ND;
 
-%% Animate board
+
+%% Simulate the markov chain for a few steps
 steps = 20;
 fps = 10;
 
-xhist = zeros(length(x0),steps+1);
-xhist(:,1) = x0;
-x = x0;
-%boardhist = zeros(length(x)/4+1,length(x)/4+1,steps+1);
+xhist = zeros(size(A,1),steps+1);
+x = eye(1,size(A,1));
+xhist(:,1) = x;
 
-%boardhist(:,:,1) = displayBoard(x);
-%pause(1/fps);
 for t = 1:steps,
     x = x*A;
     xhist(:,t+1) = x;
-    %boardhist(:,:,t+1) = displayBoard(x.^(log(.5)/log(1/40)) );
-    %pause(1/fps);
 end
 
 boardHist = makeBoard(xhist);
 
+% Play a movie (matlab) or simple animation (octave) of the simulation
 %play(pToInt(boardHist),5);
 
 %% Find steady state
@@ -124,3 +126,11 @@ boardHist = makeBoard(xhist);
 xss = V'/sum(V);
 
 imshow(pToInt(makeBoard(xss')));
+
+plot(xss);
+line([1 40], [1 1]/40, 'color','red')
+
+[xss_sorted,xss_order ]= sort(xss,'descend');
+[ num2cell(xss_sorted(1:10)') squares(xss_order(1:10)) ]
+
+
